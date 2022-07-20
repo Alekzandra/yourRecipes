8692506325e1c8c895cfc722712f34f1581b2744
Aleksandra Filipkowska <ola-filipkowska12@o2.pl>
2022-07-20 19:56:33 +0200

    Code cleaning

--- app.js
+++ app.js
@@ -24,26 +24,6 @@ const recipeSchema = new mongoose.Schema({
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
-// const recipe = new Recipe({
-//   name: "Omelette",
-//   imageURL: "https://images.unsplash.com/photo-1494597706938-de2cd7341979?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1204&q=80",
-//   description: "Omellettes are not that scare as they seem to be! Making an omelteere at home is almost easy. With few tips you can pull this off in minutes. Fill it with whatever you have on hand - freezer leftovers will do perfect for this!",
-//   prepTime: 5,
-//   cookTime: 5,
-//   noOfServings: 1,
-//   tags: ["Breakfast"],
-//   ingredients: ["3x eggs", "butter", "filling of your chosing"],
-//   instructions: ["Beat the eggs.", "Melt tle butter. Keep the temperature low and slow when cooking the eggs so the bottom doesn't get too brown or overcooked.", "Add the eggs, Let them sit for a minute, then gently use a heatproof spatula to lift the cooked part from the edges of the pan. Tilt the pan to allow the uncooked eggs to flow to the edge.", "Fill the omelette, but don't overstuff it. Cook few more seconds.", "Fold in half and slide it onto a plate. Enjoy!"]
-// });
-
-// Recipe.insertMany(recipe, function(err){
-//   if(err){
-//     console.log(err);
-//   } else {
-//     console.log("Success!");
-//   }
-// });
-
 app.get("/", function(req, res) {
   function drawRecipes(foundRecipes){
     const randomRecipes = [];
